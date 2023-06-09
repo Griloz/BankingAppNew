2a5189c841b9dfae60069153874d7b55b8909ded
Griloz <griloznev@gmail.com>
2023-06-08 23:48:27 -0400

    Added two test datas to invalid-login test

--- tests/login_test.py
+++ tests/login_test.py
@@ -7,9 +7,12 @@ import pytest
 from pages.LoginPage import LoginPage
 
 # create test cases
+
+
 def test_login_page(driver):
     # check if Login text is displayed
-    assert driver.find_element(By.CSS_SELECTOR, 'div.heading > h1').text == 'Login'
+    assert driver.find_element(
+        By.CSS_SELECTOR, 'div.heading > h1').text == 'Login'
 
 
 def test_user_login(driver):
@@ -32,12 +35,14 @@ def test_admin_login(driver):
 invalid_login_parameters = [
     ('', 'test', 'Field is required'),
     ('aa', 'test', 'Should be minimum 4 chars'),
-    ('Test', 'Test', 'Wrng username or password'),
-    # ('Test', '', 'Field is required'),
-    # ('*&^hj', 'IUH*&(*)', 'Wrong username or password'),
+    ('Test', 'Test', 'Wrong username or password'),
+    ('Test', '', 'Field is required'),
+    ('*&^hj', 'IUH*&(*)', 'Wrong username or password'),
 ]
 
 # create a parameterized test case
+
+
 @pytest.mark.parametrize("username, password, checkpoint", invalid_login_parameters)
 def test_invalid_login(driver, username, password, checkpoint):
     login_page = LoginPage(driver)
